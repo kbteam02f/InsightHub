314907e8dc2c268bdf7d1c9d477b2b74f40a7aec
Batura Konstantin <kbteam02f@gmail.com>
2025-10-26 12:43:24 +0200

    Initial commit

---
+++ src/main/java/com/example/insighthub/InsighthubApplication.java
@@ -0,0 +1,13 @@
+package com.example.insighthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsighthubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsighthubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/insighthub/InsighthubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.insighthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsighthubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
